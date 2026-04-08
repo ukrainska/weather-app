da414c7aa388adab00b9f1ed2c4c1518a45b43c4
ukrainska <215995465+ukrainska@users.noreply.github.com>
2026-04-08 22:20:12 +0900

    Fix the issue with not hidden elements

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const searchBar = document.getElementById("search-city");
 const searchBtn = document.getElementById("search-btn");
 const cityName = document.getElementById("city-name");
+const cityTemp = document.getElementById("temperature");
+const cityWind = document.getElementById("wind");
+const humidity = document.getElementById("humidity");
+const uvIndex = document.getElementById("uv");
 const weatherConditions = document.getElementById("weather-conditions");
 
 const apiKey = "59VNKM2L9TBEKL54GAS66S4W6";
@@ -23,7 +27,6 @@ async function searchWeather(location) {
 
         const data = await response.json();
         displayWeather(data);
-        console.log(data);
     }
     catch(error) {
         console.log("Fetch error: ", error);
@@ -31,8 +34,26 @@ async function searchWeather(location) {
 }
 
 function displayWeather(data) {
-    let currentWeather = data.currentConditions;
+    const currentWeather = data.currentConditions;
+
+    showText(cityName);
+
     weatherConditions.innerHTML = currentWeather.conditions;
-    console.log(data.currentConditions.conditions);
+    showText(weatherConditions);
+
+    cityTemp.innerHTML = currentWeather.temp + "°C";
+    showText(cityTemp);
+
+    cityWind.innerHTML = "Wind speed: " + currentWeather.windspeed;
+    showText(cityWind);
+
+    humidity.innerHTML = "Humidity: " + currentWeather.humidity;
+    showText(humidity);
+
+    uvIndex.innerHTML = "UV Index: " + currentWeather.uvindex;
+    showText(uvIndex);
 }
 
+function showText(element) {
+    element.removeAttribute("hidden");
+}
